8372beddddd1016366f3633ba46a879ef0befb3c
Kakayou <shigaev_1990@mail.ru>
2024-01-28 21:17:40 +0700

    При клике на кнопку "добавить", каретка остается в поле ввода

--- src/components/AddTodoItem.vue
+++ src/components/AddTodoItem.vue
@@ -2,8 +2,8 @@
   <div class="text-center m-2">
     <div v-if="formVisible">
       <button class="text-cyan-100 mr-5 text-base" @click="closeForm">X</button>
-      <input @keyup.enter="addTodo" v-model="newTodoText" class="text-neutral-600 border rounded-l-lg p-3" placeholder="Купить хлеб..."/>
-      <button @click="addTodo" class="bg-cyan-500 hover:bg-cyan-800 hover:text-neutral-300 duration-200 active:border-0 text-neutral-800 rounded-r-lg p-2 h-12">Добавить</button>
+      <input @keyup.enter="addTodo" ref="input" v-model="newTodoText" class="text-neutral-600 border rounded-l-lg p-3" placeholder="Купить хлеб..."/>
+      <button @click="addTodo(); this.$refs.input.focus()" class="bg-cyan-500 hover:bg-cyan-800 hover:text-neutral-300 duration-200 active:border-0 text-neutral-800 rounded-r-lg p-2 h-12">Добавить</button>
     </div>
     <div v-if="!formVisible">
       <button @click="showForm" class="text-lg text-cyan-100">+</button>
